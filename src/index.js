ef9b39df6bfd5786acf206a20afefffe1e9889db
Mwanziabyn <maundubyn@gmail.com>
2024-01-08 11:52:46 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,148 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>React Pizza Co.</h1>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <>
+      <div className="menu">
+        <h2>Our Menu</h2>
+
+        <p>
+          Authentic Italian cuisine. 6 creative dishes to choose from. All from
+          our stove oven, all organic, all delicious.
+        </p>
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+
+        {/* <Pizza 
+        pizzaImage="pizzas/focaccia.jpg"
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        price={12}
+      />
+      <Pizza 
+        pizzaImage="pizzas/margherita.jpg"
+        name="Pizza Magherita"
+        ingredients="Tomatos, Mozarella, Spinach and Ricotta Cheese"
+        price={10}
+  /> */}
+      </div>
+    </>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  console.log();
+  return (
+    <div className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>Name: {pizzaObj.name}</h3>
+        <p>Ingredients: {pizzaObj.ingredients}</p>
+        <span> $ {pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 9;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  //if ((hour) => openHour && hour <= closeHour) alert("We are currently open");
+  //else alert("We are currently closed");
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour} until {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Menu />
+    <Footer />
   </React.StrictMode>
 );
 
